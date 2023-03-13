75c771fbce5e7061b9514242746e7af9477edeb9
RedJocker <mauricio.guaruja@gmail.com>
2023-03-13 12:01:35 -0300

    removing solution from main

--- Simple Bank Manager/stage1/src/main/java/org/hyperskill/simplebankmanager/MainActivity.kt
+++ Simple Bank Manager/stage1/src/main/java/org/hyperskill/simplebankmanager/MainActivity.kt
@@ -2,24 +2,10 @@ package org.hyperskill.simplebankmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import org.hyperskill.simplebankmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), LoginFragment.LoginValidator {
-
-    lateinit var mainBinding: ActivityMainBinding
-    lateinit var username: String
-    lateinit var password: String
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        username = intent.extras?.getString("username") ?: "Lara"
-        password = intent.extras?.getString("password") ?: "1234"
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainBinding.root)
-    }
-
-    override fun isValidLogin(username: String, password: String): Boolean {
-        return this.username == username
-                && this.password == password
+        setContentView(R.layout.activity_main)
     }
 }
